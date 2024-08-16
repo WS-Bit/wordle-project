8d468ac6a385d68c385cc176527c15af5c81632f
wmsexton <wmsexton@btinternet.com>
2024-08-16 17:18:03 +0100

    added 1000 more words, added a win count to track wins

--- script/app.js
+++ script/app.js
@@ -1,5 +1,3 @@
-
-
 console.log("java script is working")
 const wordArray =  [
 'WHICH', 'THERE', 'THEIR', 'ABOUT', 'WOULD', 'THESE', 'OTHER', 'WORDS', 'COULD', 'WRITE',
@@ -101,7 +99,55 @@ const wordArray =  [
 'CAMEL', 'VINES', 'PICKS', 'NAVAL', 'PURSE', 'RIGID', 'CRAWL', 'TOAST', 'SOILS', 'SAUCE',
 'BASIN', 'PONDS', 'TWINS', 'WRIST', 'FLUID', 'POOLS', 'BRAND', 'STALK', 'ROBOT', 'REEDS',
 'HOOFS', 'BUSES', 'SHEER', 'GRIEF', 'BLOOM', 'DWELT', 'MELTS', 'RISEN', 'FLAGS', 'KNELT',
-'FIBER', 'ROOFS', 'FREED', 'ARMOR', 'PILES', 'AIMED', 'ALGAE', 'TWIGS', 'LEMON', 'ATONE'
+'FIBER', 'ROOFS', 'FREED', 'ARMOR', 'PILES', 'AIMED', 'ALGAE', 'TWIGS', 'LEMON', 'ATONE',
+'DRUNK', 'RESTS', 'CHILL', 'SLAIN', 'PANIC', 'CORDS', 'TUNED', 'CRISP', 'LEDGE', 'DIVED',
+'SWAMP', 'CLUNG', 'STOLE', 'MOLDS', 'YARNS', 'LIVER', 'GAUGE', 'BREED', 'STOOL', 'GULLS',
+'AWOKE', 'GROSS', 'DIARY', 'RAILS', 'BELLY', 'TREND', 'FLASK', 'STAKE', 'FRIED', 'DRAWS',
+'ACTOR', 'HANDY', 'BOWLS', 'HASTE', 'SCOPE', 'DEALS', 'KNOTS', 'MOONS', 'ESSAY', 'THUMP',
+'HANGS', 'BLISS', 'DEALT', 'GAINS', 'BOMBS', 'CLOWN', 'PALMS', 'CONES', 'ROAST', 'TIDAL',
+'BORED', 'CHANT', 'ACIDS', 'DOUGH', 'CAMPS', 'SWORE', 'LOVER', 'HOOKS', 'MALES', 'COCOA',
+'PUNCH', 'AWARD', 'REINS', 'NINTH', 'NOSES', 'LINKS', 'DRAIN', 'FILLS', 'NYLON', 'LUNAR',
+'PULSE', 'FLOWN', 'ELBOW', 'FATAL', 'SITES', 'MOTHS', 'MEATS', 'FOXES', 'MINED', 'ATTIC',
+'FIERY', 'MOUNT', 'USAGE', 'SWEAR', 'SNOWY', 'RUSTY', 'SCARE', 'TRAPS', 'RELAX', 'REACT',
+'VALID', 'ROBIN', 'CEASE', 'GILLS', 'PRIOR', 'SAFER', 'POLIO', 'LOYAL', 'SWELL', 'SALTY',
+'MARSH', 'VAGUE', 'WEAVE', 'MOUND', 'SEALS', 'MULES', 'VIRUS', 'SCOUT', 'ACUTE', 'WINDY',
+'STOUT', 'FOLDS', 'SEIZE', 'HILLY', 'JOINS', 'PLUCK', 'STACK', 'LORDS', 'DUNES', 'BURRO',
+'HAWKS', 'TROUT', 'FEEDS', 'SCARF', 'HALLS', 'COALS', 'TOWEL', 'SOULS', 'ELECT', 'BUGGY',
+'PUMPS', 'LOANS', 'SPINS', 'FILES', 'OXIDE', 'PAINS', 'PHOTO', 'RIVAL', 'FLATS', 'SYRUP',
+'RODEO', 'SANDS', 'MOOSE', 'PINTS', 'CURLY', 'COMIC', 'CLOAK', 'ONION', 'CLAMS', 'SCRAP',
+'DIDST', 'COUCH', 'CODES', 'FAILS', 'OUNCE', 'LODGE', 'GREET', 'GYPSY', 'UTTER', 'PAVED',
+'ZONES', 'FOURS', 'ALLEY', 'TILES', 'BLESS', 'CREST', 'ELDER', 'KILLS', 'YEAST', 'ERECT',
+'BUGLE', 'MEDAL', 'ROLES', 'HOUND', 'SNAIL', 'ALTER', 'ANKLE', 'RELAY', 'LOOPS', 'ZEROS',
+'BITES', 'MODES', 'DEBTS', 'REALM', 'GLOVE', 'RAYON', 'SWIMS', 'POKED', 'STRAY', 'LIFTS',
+'MAKER', 'LUMPS', 'GRAZE', 'DREAD', 'BARNS', 'DOCKS', 'MASTS', 'POURS', 'WHARF', 'CURSE',
+'PLUMP', 'ROBES', 'SEEKS', 'CEDAR', 'CURLS', 'JOLLY', 'MYTHS', 'CAGES', 'GLOOM', 'LOCKS',
+'PEDAL', 'BEETS', 'CROWS', 'ANODE', 'SLASH', 'CREEP', 'ROWED', 'CHIPS', 'FISTS', 'WINES',
+'CARES', 'VALVE', 'NEWER', 'MOTEL', 'IVORY', 'NECKS', 'CLAMP', 'BARGE', 'BLUES', 'ALIEN',
+'FROWN', 'STRAP', 'CREWS', 'SHACK', 'GONNA', 'SAVES', 'STUMP', 'FERRY', 'IDOLS', 'COOKS',
+'JUICY', 'GLARE', 'CARTS', 'ALLOY', 'BULBS', 'LAWN', 'LASTS', 'FUELS', 'ODDLY', 'CRANE',
+'FILED', 'WEIRD', 'SHAWL', 'SLIPS', 'TROOP', 'BOLTS', 'SUITE', 'SLEEK', 'QUILT', 'TRAMP',
+'BLAZE', 'ATLAS', 'ODORS', 'SCRUB', 'CRABS', 'PROBE', 'LOGIC', 'ADOBE', 'EXILE', 'REBEL',
+'GRIND', 'STING', 'SPINE', 'CLING', 'DESKS', 'GROVE', 'LEAPS', 'PROSE', 'LOFTY', 'AGONY',
+'SNARE', 'TUSKS', 'BULLS', 'MOODS', 'HUMID', 'FINER', 'DIMLY', 'PLANK', 'CHINA', 'PINES',
+'GUILT', 'SACKS', 'BRACE', 'QUOTE', 'LATHE', 'GAILY', 'FONTS', 'SCALP', 'ADOPT', 'FOGGY',
+'FERNS', 'GRAMS', 'CLUMP', 'PERCH', 'TUMOR', 'TEENS', 'CRANK', 'FABLE', 'HEDGE', 'GENES',
+'SOBER', 'BOAST', 'TRACT', 'CIGAR', 'UNITE', 'OWING', 'THIGH', 'HAIKU', 'SWISH', 'DIKES',
+'WEDGE', 'BOOTH', 'EASED', 'FRAIL', 'COUGH', 'TOMBS', 'DARTS', 'FORTS', 'CHOIR', 'POUCH',
+'PINCH', 'HAIRY', 'BUYER', 'TORCH', 'VIGOR', 'WALTZ', 'HEATS', 'HERBS', 'USERS', 'FLINT',
+'CLICK', 'MADAM', 'BLEAK', 'BLUNT', 'AIDED', 'LACKS', 'MASKS', 'WADED', 'RISKS', 'NURSE',
+'CHAOS', 'SEWED', 'CURED', 'AMPLE', 'LEASE', 'STEAK', 'SINKS', 'MERIT', 'BLUFF', 'BATHE',
+'GLEAM', 'BONUS', 'COLTS', 'SHEAR', 'GLAND', 'SILKY', 'SKATE', 'BIRCH', 'ANVIL', 'SLEDS',
+'GROAN', 'MAIDS', 'MEETS', 'SPECK', 'HYMNS', 'HINTS', 'DROWN', 'BOSOM', 'SLICK', 'QUEST',
+'COILS', 'SPIED', 'SNOWS', 'STEAD', 'SNACK', 'PLOWS', 'BLOND', 'TAMED', 'THORN', 'WAITS',
+'GLUED', 'BANJO', 'TEASE', 'ARENA', 'BULKY', 'CARVE', 'STUNT', 'WARMS', 'SHADY', 'RAZOR',
+'FOLLY', 'LEAFY', 'NOTCH', 'FOOLS', 'OTTER', 'PEARS', 'FLUSH', 'GENUS', 'ACHED', 'FIVES',
+'FLAPS', 'SPOUT', 'SMOTE', 'FUMES', 'ADAPT', 'CUFFS', 'TASTY', 'STOOP', 'CLIPS', 'DISKS',
+'SNIFF', 'LANES', 'BRISK', 'IMPLY', 'DEMON', 'SUPER', 'FURRY', 'RAGED', 'GROWL', 'TEXTS',
+'HARDY', 'STUNG', 'TYPED', 'HATES', 'WISER', 'TIMID', 'SERUM', 'BEAKS', 'ROTOR', 'CASTS',
+'BATHS', 'GLIDE', 'PLOTS', 'TRAIT', 'RESIN', 'SLUMS', 'LYRIC', 'PUFFS', 'DECKS', 'BROOD',
+'MOURN', 'ALoft', 'ABUSE', 'WHIRL', 'EDGED', 'OVARY', 'QUACK', 'HEAPS', 'SLANG', 'AWAIT',
+'CIVIC', 'SAINT', 'BEVEL', 'SONAR', 'AUNTS', 'PACKS', 'FROZE', 'TONIC', 'CORPS', 'SWARM',
+'FRANK', 'REPAY', 'GAUNT', 'WIRED'
 ];
 const wordGrid = document.querySelector(".grid");
 const wordCellCount = 5;
@@ -114,6 +160,8 @@ const submitButton = document.querySelector("#submit-button");
 const guessesElement = document.querySelector("#guesses");
 let guesses = 6;
 let currentRowIndex = 0;
+let wins = localStorage.getItem('wordleWins');
+    wins = wins ? parseInt(wins) : 0;
 
 function createRow() {
 const row = [];
@@ -222,7 +270,24 @@ document.body.appendChild(gameOverContainer);
 submitButton.removeEventListener('click', checkForWinner);
 }
 
+function initWinCounter() {
+  let wins = localStorage.getItem('wordleWins');
+  wins = wins ? parseInt(wins) : 0;
+  updateWinDisplay(wins);
+}
 
-submitButton.addEventListener('click', checkForWinner)
+function incrementWins() {
+  let wins = localStorage.getItem('wordleWins');
+  wins = wins ? parseInt(wins) : 0;
+  wins++;
+  localStorage.setItem('wordleWins', wins);
+  updateWinDisplay(wins);
+}
 
+function updateWinDisplay(wins) {
+  document.getElementById('win-count').textContent = wins;
+}
+
+submitButton.addEventListener('click', checkForWinner, incrementWins)
+localStorage.setItem('wordleWins', wins);
 
