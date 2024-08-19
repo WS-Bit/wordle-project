cbafe914fd5116ef86b6d7dd894f3c413f7f4ac4
wmsexton <wmsexton@btinternet.com>
2024-08-19 16:24:40 +0100

    hard mode engaged, grid updates acordingly, changed styling of grid, styling overall changed, font colour chnaged, attempt 1 to import words from seperate file was a fail so redoing tomorrow

--- words/wordList.js
+++ words/wordList.js
@@ -1,6 +1,4 @@
-console.log("Hello world! Java script is working!")
-
-const easyWordArray =  [
+export const easyWordArray =  [
 'WHICH', 'THERE', 'THEIR', 'ABOUT', 'WOULD', 'OTHER', 'COULD', 'WRITE',
 'FIRST', 'WATER', 'AFTER', 'WHERE', 'RIGHT', 'THINK', 'YEARS', 'PLACE',
 'SOUND', 'GREAT', 'AGAIN', 'STILL', 'EVERY', 'SMALL', 'FOUND', 'THOSE',
@@ -79,13 +77,13 @@ const easyWordArray =  [
 'SWEET', 'TREND', 'THROB', 'TREAT', 'THROB', 'THRUM', 'THUMB', 'THORN',
 'TREND', 'TRIAL', 'TRICK', 'TROOP', 'TREAD', 'TROUT', 'TRIAL', 'TROUT',
 'TWEAK', 'TWIST', 'TUMOR', 'TURBO', 'TWANG', 'TWINE', 'TWIRL', 'TWEET',
-'TYRANT', 'TYPIC', 'UNCLE', 'UNDER', 'UNITY', 'UNTIL', 'UPPER', 'UPSET',
-'URBAN', 'USAGE', 'USEFUL', 'UTTER', 'VAGUE', 'VAULT', 'VENGE', 'VENOM',
-'VERGE', 'VEXED', 'VICAR', 'VIGOR', 'VINTAGE', 'VIRUS', 'VISIT', 'VITAL',
+'TYPIC', 'UNCLE', 'UNDER', 'UNITY', 'UNTIL', 'UPPER', 'UPSET',
+'URBAN', 'USAGE', 'UTTER', 'VAGUE', 'VAULT', 'VENGE', 'VENOM',
+'VERGE', 'VEXED', 'VICAR', 'VIGOR', 'VIRUS', 'VISIT', 'VITAL',
 'VOCAL', 'VOGUE', 'VOICE', 'VOWEL', 'WAGER', 'WALKS', 'WASTE', 'WATER',
 'WEARY', 'WEAVE', 'WEDGE', 'WEIGH', 'WELLS', 'WHEAT', 'WHILE', 'WHISK',
-'WHITE', 'WHOLE', 'WIDEN', 'WIDER', 'WIDTH', 'WILDLY', 'WINCE', 'WINDS',
-'WISDOM', 'WITTY', 'WORRY', 'WOUND', 'WOVEN', 'WRACK', 'WRATH', 'WRONG',
+'WHITE', 'WHOLE', 'WIDEN', 'WIDER', 'WIDTH', 'WINCE', 'WINDS',
+'WITTY', 'WORRY', 'WOUND', 'WOVEN', 'WRACK', 'WRATH', 'WRONG',
 'YACHT', 'YEARN', 'YEAST', 'YIELD', 'YOUNG', 'YOUTH', 'ZEBRA', 'ZESTY',
 'SHINE', 'YOUTH', 'THORN', 'THRUM', 'HOARD', 'LIVER', 'TREND', 'HANDY',
 'KINDY', 'RULER', 'SPEAR', 'SHONE', 'HORNY', 'PLANE', 'GAMED', 'MILER',
@@ -96,7 +94,7 @@ const easyWordArray =  [
 'YOUNG', 'STUMP', 'GRAVE'
 ];
 
-const hardWordArray =  
+export const hardWordArray =  
 [
   "ABILITY", "ABSENCE", "ACADEMY", "ACCOUNT", "ADVANCE", "ADVERSE", "ADVISER",
   "ADOPTED", "ADVISER", "ADVISED", "AMAZING", "ANALYZE", "ANCIENT", "ANXIETY",
@@ -146,7 +144,7 @@ const hardWordArray =
   "SUPPOSE", "SUPREME", "SURFACE", "SURGERY", "SURPLUS", "SURVIVE", "SUSPECT",
   "SUSTAIN", "SYMPTOM", "TEACHER", "TELLING", "TENSION", "THEATRE", "THERAPY",
   "THOUGHT", "THROUGH", "TONIGHT", "TOTALLY", "TOURISM", "TRADING", "TRAFFIC",
-  "TRIGGER", "TROPICAL", "TURNING", "TYPICAL", "UNCLEAR", "UNDERGO", "UPGRADE",
+  "TRIGGER", "TURNING", "TYPICAL", "UNCLEAR", "UNDERGO", "UPGRADE",
   "UTILITY", "VACCINE", "VARIETY", "VARIOUS", "VEHICLE", "VENTURE", "VERDICT",
   "VERSION", "VETERAN", "VICTORY", "VILLAGE", "VISIBLE", "WAITING", "WARNING",
   "WARRANT", "WEARING", "WEATHER", "WEBCAST", "WEDDING", "WEEKEND", "WELFARE",
@@ -155,309 +153,3 @@ const hardWordArray =
   "KITCHEN", "LACKING", "LANDING", "LAPTOPS", "LARGELY", "LASTING", "LAWYERS",
   "LEASING", "LEATHER", "LEAVING", "LEGALLY", "LIBERAL"
 ];
-
-// ! CONSTS
-
-const wordGrid = document.querySelector(".grid");
-let wordCellCount = 5;
-const gridRows = [];
-const randomWordIndex = Math.floor(Math.random() * easyWordArray.length);
-let CORRECT_GUESS = easyWordArray[randomWordIndex];
-// console.log(CORRECT_GUESS);
-let correctArray = CORRECT_GUESS.split("");
-
-const submitButton = document.querySelector("#submit-button");
-const inputBox = document.querySelector(".text-input");
-const guessesElement = document.querySelector("#guesses");
-const playAgainButton = document.getElementById('play-again');
-const rules = document.querySelector(".rules");
-const audio = document.querySelector("audio");
-audio.volume = 0.25;
-
-let guesses = 6;
-let currentRowIndex = 0;
-let wins = localStorage.getItem('wordleWins');
-    wins = wins ? parseInt(wins) : 0;
-
-let gameMode ='easy';
-
-// ! ROW CREATION
-
-function createRow() {
-const row = [];
-for (let i = 0; i < wordCellCount; i++) {
-    const cell = document.createElement("div");
-    cell.setAttribute('data-index', i);
-    wordGrid.appendChild(cell);
-    cell.innerText = "";
-    cell.classList.add('word-cell');
-    row.push(cell);
-}
-gridRows.push(row);
-}
-
-function createGrid() {
-  wordGrid.innerHTML = '';
-  gridRows.length = 0;
-  
-  if (gameMode === 'easy') {
-      wordGrid.classList.remove('hard-mode');
-      wordGrid.classList.add('easy-mode');
-  } else {
-      wordGrid.classList.remove('easy-mode');
-      wordGrid.classList.add('hard-mode');
-  }
-
-  // Create the rows
-  for (let i = 0; i < 6; i++) {
-      createRow();
-  }
-}
-
-createGrid();
-
-//! MAIN FUNCTION
-
-function checkForWinner() {
-  const GUESS = document.querySelector("input").value.toUpperCase();
-
-  const validWordArray = gameMode === 'easy' ? easyWordArray : hardWordArray;
-    if (!validWordArray.includes(GUESS)) {
-        alert('The word is not in the list of valid words.');
-        return;
-    }
-
-  audio.play();
-  const guessArray = GUESS.split("");
-  const displayGuessArray = [...guessArray];
-  let correctArray = [...CORRECT_GUESS.split("")];
-  const resultsArray = new Array(wordCellCount).fill('incorrect');
-
-  for (let i = 0; i < wordCellCount; i++) {
-      if (guessArray[i] === correctArray[i]) {
-          resultsArray[i] = 'correct';
-          correctArray[i] = null;
-          guessArray[i] = null;
-      }
-  }
-
-  for (let i = 0; i < wordCellCount; i++) {
-      if (guessArray[i] !== null) {
-          let index = correctArray.indexOf(guessArray[i]);
-          if (index !== -1) {
-              resultsArray[i] = 'misplaced';
-              correctArray[index] = null; 
-          }
-      }
-  }
-
-  colourHandling(displayGuessArray, resultsArray);
-  
-  if (resultsArray.every((result) => result === 'correct')) {
-      winGame();
-  } else {
-      guesses--;
-      guessesElement.innerText = guesses;
-      if (guesses === 0) {
-          loseGame();
-      }
-      currentRowIndex++;
-  }
-}
-
-//! COLOUR CONTROL
-
-function colourHandling(guessArray, resultsArray) {
-gridRows[currentRowIndex].forEach((cell, index) => {
-    cell.innerText = guessArray[index];
-    cell.classList.add(resultsArray[index]);
-});
-}
-
-//! WIN AND LOSS DISPLAY 
-
-function winGame() {
-  incrementWins()
-  gameActive = false;
-  const winGameMessage = document.createElement('div');
-
-  winGameMessage.id = 'winGameMessage'
-  winGameMessage.innerHTML = 'You win!';
-  winGameMessage.style.position = 'absolute';
-  winGameMessage.style.top = '50%';
-  winGameMessage.style.left = '50%';
-  winGameMessage.style.transform = 'translate(-50%, -50%)';
-  winGameMessage.style.backgroundColor = 'rgb(113, 255, 168)';
-  winGameMessage.style.fontSize = '48px';
-  winGameMessage.style.padding = '20px';
-  winGameMessage.style.fontWeight = 'bold'; 
-  winGameMessage.style.border = '1px solid black';
-  document.body.appendChild(winGameMessage);
-  submitButton.removeEventListener('click', checkForWinner)
-  playAgainButton.style.display = 'block';
-}
-
-function loseGame() {
-  gameActive = false;
-  eraseWins();
-  const gameOverContainer = document.createElement('div');
-
-  gameOverContainer.id = 'gameOverContainer';
-  gameOverContainer.style.position = 'absolute';
-  gameOverContainer.style.top = '50%';
-  gameOverContainer.style.left = '50%';
-  gameOverContainer.style.transform = 'translate(-50%, -50%)';
-  gameOverContainer.style.backgroundColor = 'rgb(255, 91, 91)';
-  gameOverContainer.style.fontSize = '28px';
-  gameOverContainer.style.padding = '20px';
-  gameOverContainer.style.fontWeight = 'bold';
-  gameOverContainer.style.textAlign = 'center';
-  gameOverContainer.style.border = '1px solid black';
-
-  const gameOverMessage = document.createElement('div');
-  gameOverMessage.innerHTML = 'Game Over!';
-  gameOverMessage.style.marginBottom = '10px'; 
-
-  const chosenWordMessage = document.createElement('div');
-  const chosenWord = 'The chosen word was: ' + CORRECT_GUESS;
-  chosenWordMessage.innerHTML = chosenWord;
-
-  gameOverContainer.appendChild(gameOverMessage);
-  gameOverContainer.appendChild(chosenWordMessage);
-  document.body.appendChild(gameOverContainer);
-
-  submitButton.removeEventListener('click', checkForWinner);
-  playAgainButton.style.display = 'block';
-}
-
-playAgainButton.addEventListener('click', () => {
-  resetGame();
-  playAgainButton.style.display = 'none';
-});
-
-// ! RESET GAME
-
-function resetGame() {
-  guesses = 6;
-  currentRowIndex = 0;
-  inputBox.setAttribute('maxlength', gameMode === 'easy' ? 5:7)
-
-  let randomWordIndex = Math.floor(Math.random() * (gameMode === 'easy' ? easyWordArray.length : hardWordArray.length));
-  CORRECT_GUESS = gameMode === 'easy' ? easyWordArray[randomWordIndex] : hardWordArray[randomWordIndex];
-  wordCellCount = gameMode === 'easy' ? 5 : 7;
-
-  console.log(CORRECT_GUESS);
-
-  correctArray = CORRECT_GUESS.split("");
-
-  createGrid();
-  
-  guessesElement.innerText = guesses;
-  document.querySelector("input").value = "";
-  
-  const gameOverMessages = document.querySelectorAll('#gameOverContainer, div[style*="position: absolute"]');
-    gameOverMessages.forEach(message => message.remove());
-  
-  const winMessage = document.getElementById('winGameMessage');
-    if (winMessage) winMessage.remove();
-  
-  submitButton.addEventListener('click', checkForWinner);
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  initWinCounter();
-});
-
-function initWinCounter() {
-  let wins = localStorage.getItem('wordleWins');
-  console.log('Initial wins from localStorage:', wins);
-  wins = wins ? parseInt(wins) : 0;
-  updateWinDisplay(wins);
-}
-
-function incrementWins() {
-  let wins = localStorage.getItem('wordleWins');
-  console.log('Wins before increment:', wins);
-  wins = wins ? parseInt(wins) : 0;
-  wins++;
-  localStorage.setItem('wordleWins', wins);
-  console.log('Wins after increment:', wins);
-  updateWinDisplay(wins);
-}
-
-function eraseWins() {
-  let wins = localStorage.getItem('wordleWins');
-  wins = wins ? parseInt(wins) : 0;
-  wins = 0;
-  localStorage.setItem('wordleWins', wins);
-  updateWinDisplay(wins);
-}
-
-function updateWinDisplay(wins) {
-  document.getElementById('win-count').textContent = wins;
-}
-
-function displayRules() {
-  const rulesBox = document.createElement('div');
-
-  rulesBox.id = 'rulesBox';
-  rulesBox.style.position = 'absolute';
-  rulesBox.style.top = '50%';
-  rulesBox.style.left = '50%';
-  rulesBox.style.transform = 'translate(-50%, -50%)';
-  rulesBox.style.backgroundColor = 'rgb(250, 223, 200)';
-  rulesBox.style.fontSize = '28px';
-  rulesBox.style.padding = '20px';
-  rulesBox.style.fontWeight = 'bold';
-  rulesBox.style.textAlign = 'center';
-  rulesBox.style.border = '1px solid black';
-  rulesBox.style.color = 'rgb(138, 138, 138)'
-
-  const rulesMessage = document.createElement('div');
-  rulesMessage.innerHTML = 'Enter a 5 or 7 letter word based on the selected mode. <br> Green = Correct letter and placement, <br> Yellow = Correct letter, wrong placement, <br> Red = Wrong letter!';
-  rulesMessage.style.marginBottom = '10px'; 
-  rulesBox.appendChild(rulesMessage);
-  document.body.appendChild(rulesBox);
-}
-
-function hideRules() {
-  const rulesBox = document.getElementById('rulesBox');
-  if (rulesBox) {
-    document.body.removeChild(rulesBox);
-  }
-}
-
-rules.addEventListener('mouseover', displayRules);
-rules.addEventListener('mouseout', hideRules);
-
-submitButton.addEventListener('click', function(event) {
-  if (event) {
-    event.preventDefault();
-    checkForWinner();
-    inputBox.value = '';
-  }
-});
-
-inputBox.addEventListener('keyup', function(event) {
-  if (event.key === 'Enter') {
-    event.preventDefault();
-    checkForWinner();
-    inputBox.value = '';
-  }
-});
-
-localStorage.setItem('wordleWins', wins);
-
-
-// ! EASY AND HARD MODE 
-
-document.querySelector('.easy-mode-button').addEventListener('click', () => {
-  gameMode = 'easy';
-  resetGame();
-});
-
-document.querySelector('.hard-mode-button').addEventListener('click', () => {
-  gameMode = 'hard';
-  resetGame();
-});
-
